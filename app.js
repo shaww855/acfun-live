52a5674e91353960ec5ce7cb268fcfd772fc2a40
shaww855 <haseeer@gmail>
2021-01-19 13:44:34 +0800

    错误处理&&超时

--- app.js
+++ app.js
@@ -45,9 +45,8 @@ function formartDate (time) {
 /**
  * 开始监控室
  * @param {Object} page 页面
- * @param {Object} browser 浏览器
  */
-async function startMonitor (page, browser) {
+async function startMonitor (page) {
   console.groupCollapsed('第', times + 1, '次检查直播状态')
   console.log(formartDate(new Date));
   let isLiveList = await page.evaluate(async () => {
@@ -135,7 +134,7 @@ async function startMonitor (page, browser) {
     })
   });
   // console.log('isLiveList', isLiveList);
-  DDVup(await browser.pages(), isLiveList.filter(e => e.watch), browser)
+  DDVup(await page.browser().pages(), isLiveList.filter(e => e.watch))
   console.groupEnd('第', times + 1, '次检查直播状态')
   times ++
 }
@@ -152,13 +151,13 @@ function getUidByLink (link) {
  * 开启DD监控室
  * @param {Object} pages 已打开的页面对象
  * @param {Array} liveUidList 直播中的用户uid数组
- * @param {Object} browser 浏览器对象
  */
-async function DDVup (pages, liveUpinfo, browser) {
+async function DDVup (pages, liveUpinfo) {
   console.log('---');
   liveUpinfo.forEach(e => {
-    console.log('up主：', e.uperId, e.uperName, e.title, `开播于 ${formartDate(e.createTime)}`);
-    console.log('粉丝牌：', e.level, e.clubName, `(${e.limit})`,`获取于 ${formartDate(e.joinClubTime)}`,);
+    console.log('主播：', e.uperId, e.uperName, `开播于 ${formartDate(e.createTime)}`);
+    console.log('标题：', e.title);
+    console.log('牌子：', e.level, e.clubName, `(${e.limit})`,`获取于 ${formartDate(e.joinClubTime)}`,);
     console.log('---');
   })
   let liveUidList = liveUpinfo.map(e => e.authorId)
@@ -171,16 +170,21 @@ async function DDVup (pages, liveUpinfo, browser) {
       // 直播仍继续
       openedUid.push(uid)
     } else {
-      console.log(`退出 ${liveUpinfo[index].uperName} 的直播间`);
+      console.log('退出直播间', liveUpinfo[index].uperName);
       page.close()
     }
   })
 
   liveUidList.filter(e => !openedUid.includes(e)).forEach((uid, index) => {
-    console.log(`进入 ${liveUpinfo[index].uperName} 的直播间`);
-    browser.newPage().then(async page => {
+    console.log('进入直播间', liveUpinfo[index].uperName);
+    pages[0].browser().newPage().then(async page => {
       await page.setRequestInterception(true);
-
+      page.setDefaultTimeout(config.defaultTimeout * 1000 * 60)
+      page.on('error', async error => {
+        console.log(error);
+        await browser.close()
+        process.exit(1)
+      })
       page.on('request', request => {
         if (request.resourceType() === 'image') {
           request.continue({
@@ -202,6 +206,19 @@ async function DDVup (pages, liveUpinfo, browser) {
   })
 }
 
+process.on('uncaughtException', err => {
+  console.error('有一个未捕获的错误', err)
+  Browser.close()
+  process.exit(1) //强制性的（根据 Node.js 文档）
+})
+process.on("unhandledRejection", err => {
+  console.error('有一个未拒绝的错误', err)
+  Browser.close()
+  process.exit(1) //强制性的（根据 Node.js 文档）
+});
+
+
+let Browser = null
 puppeteer.launch({
   // devtools: true,
   product: 'chrome',
@@ -211,10 +228,19 @@ puppeteer.launch({
   // },
   args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-extensions']
 }).then(async browser => {
+  Browser = browser
   const pageList = await browser.pages()
-  const mainPage = pageList[0]
-  await mainPage.setRequestInterception(true);
-  mainPage.on('request', request => {
+  const page = pageList[0]
+  await page.setRequestInterception(true);
+  page.setDefaultTimeout(config.defaultTimeout * 1000 * 60)
+
+  page.on('error', async error => {
+    console.log(error);
+    await browser.close()
+    process.exit(1)
+  })
+
+  page.on('request', request => {
     if (request.resourceType() === 'image') {
       // 随便塞一个小图片，减少资源占用
       request.continue({
@@ -226,18 +252,18 @@ puppeteer.launch({
 
   // 开始登录
   if (config.cookies !== '') {
-    await userLoginByCookies(mainPage)
-    await mainPage.goto('https://www.acfun.cn')
+    await userLoginByCookies(page)
+    await page.goto('https://www.acfun.cn')
   } else {
-    await mainPage.goto('https://www.acfun.cn')
-    await userLogin(mainPage)
+    await page.goto('https://www.acfun.cn')
+    await userLogin(page)
   }
-  mainPage.evaluate(() => {
+  page.evaluate(() => {
     document.write('')
   });
 
   // 检查登录状态
-  let personalInfo = await mainPage.waitForFunction(() => {
+  let personalInfo = await page.waitForFunction(() => {
     return fetch(
       'https://www.acfun.cn/rest/pc-direct/user/personalInfo',
       {
@@ -249,14 +275,15 @@ puppeteer.launch({
       return err
     })
   })
-  personalInfo = await personalInfo.jsonValue()
-  if (personalInfo.info) {
-    console.log('登录用户：', personalInfo.info.userName, personalInfo.info.userId);
+  let personalInfoJson = await personalInfo.jsonValue()
+  personalInfo.dispose()
+  if (personalInfoJson.info) {
+    console.log('登录用户：', personalInfoJson.info.userName, personalInfoJson.info.userId);
     // 起飞
-    startMonitor(mainPage, browser)
+    startMonitor(page)
     setInterval(() => {
-      startMonitor(mainPage, browser)
-    }, 1000 * 60 * config.timeOut)
+      startMonitor(page)
+    }, 1000 * 60 * config.checkLiveTimeout)
   } else {
     console.log('登录失败，请检查配置');
   }
