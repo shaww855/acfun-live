1582f459e6336d0eb8da92aa933988f33e6e8426
shaww855 <haseeer@gmail.com>
2021-06-27 10:49:45 +0800

    开播通知调整

--- notification/index.js
+++ notification/index.js
@@ -1,14 +1,19 @@
-const { serverIndex, checkLiveTimeout, notification, iftttKey, barkKey } = require('../config.json')
+const { checkLiveTimeout, notification, iftttKey, barkKey } = require('../config.json')
 const IFTTT = require('./ifttt')
 let data = {}
 
 module.exports = (liveUperInfo) => {
   if (!notification) return
-  if (new Date().getHours() === 0) {
-    // 每天0点清空发送记录
+  const hours = new Date().getHours()
+  if (hours === 0 && new Date().getMinutes() < 10) {
+    // 每天0点10分触发 清空
     data = {}
   }
-  
+  if (hours < 6) {
+    console.log('开播通知 0~6点默认不发送');
+    return
+  }
+
   let sendFn = undefined
   if (iftttKey !== '') {
     sendFn = IFTTT
@@ -27,18 +32,18 @@ module.exports = (liveUperInfo) => {
     const target = data[element.uperId]
     if (target !== undefined) {
       // 已经通知过
-      return  
+      if ((new Date().getTime() - element.createTime) > 1000 * 60 * checkLiveTimeout) {
+        // 开播超过十分钟
+        return
+      }
+      // 虽然通知过,但配置的时间内再次开播,也会进行通知
     }
-    // if ((new Date().getTime() - element.createTime) > 1000 * 60 * checkLiveTimeout) {
-    //   // 开播超过十分钟
-    //   return
-    // }
     needToSend.push(element)
     data[element.uperId] = element
   });
 
   if (needToSend.length === 0) return
-  
+
   sendFn(needToSend).then(res => {
     console.log('开播通知 发送成功');
   }).catch(err => {
