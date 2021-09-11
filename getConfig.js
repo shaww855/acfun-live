ebfc38968c9c0071aa8ceefbf7af2539a6bb5546
shilx <haseeer@gmail.com>
2021-09-11 15:18:37 +0800

    读取config方式

--- getConfig.js
+++ getConfig.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
-const path = require("path");
 
-const configPath = path.join(process.execPath, "../","./config.json");
-let isConfigExist = fs.existsSync(configPath);
-
+const configPath = __dirname + "/config.json"
 let config = null
-if (isConfigExist) {
+if (fs.existsSync(configPath)) {
   config = JSON.parse(fs.readFileSync(configPath, "utf8"));
 } else {
   throw ' config.json 文件不存在，请检查'
