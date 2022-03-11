74d5b612a05709270f222364d088eb780b22532c
shaww855 <haseeer@gmail>
2022-03-11 16:59:35 +0800

    尝试inquirer

--- util.js
+++ util.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const fs = require("fs");
+const inquirer = require('inquirer');
 
 /**
 * 补零
@@ -77,26 +78,120 @@ const getConfig = () => {
     console.log(`= config.json config 已读取 =`);
     return config
   } else {
-    throw '未找到config.json';
+    // throw '未找到config.json';
+    console.log('未找到config.json，或文件已损坏！');
+    console.log('utils');
+    return null
+    // await inquirer.prompt([{
+    //   type: 'confirm',
+    //   name: 'create',
+    //   message: "未找到config.json，或文件已损坏！是否重新建立？",
+    // }]).then(answers => {
+    //   if (answers.create) {
+    //     return configQuestion()
+    //   } else {
+    //     console.log('程序即将关闭...');
+    //     setTimeout(() => {
+    //       process.exit(0)
+    //     }, 1000)
+    //   }
+    // })
   }
 }
 
-const setConfig = (val = '', prop) => {
-  if (prop === 'cookies') {
-    if (val === '') {
-      config.cookies = ''
-    } else {
-      config.cookies = val.map(e => ({
-        name: e.name,
-        value: e.value,
-        domain: e.domain
-      }))
+const setConfig = ({
+  prop = null,
+  value = '',
+  userConfig = {}
+}) => {
+  if (prop === null) {
+    config = {
+      ...config,
+      ...userConfig
     }
+  } else if (prop === 'cookies') {
+    config.cookies = val.map(e => ({
+      name: e.name,
+      value: e.value,
+      domain: e.domain
+    }))
   } else {
-    config[prop] = val
+    config[prop] = value
   }
   fs.writeFileSync(configPath, JSON.stringify(config, null, 4));
   console.log(`= config.json cookies 已保存 =`);
+}
+
+const defaultConfig = {
+  "account": "",
+  "password": "",
+  "debug": false,
+  "checkLiveTimeout": 10,
+  "likeBtnTimeout": 0,
+  "defaultTimeout": 5,
+  "executablePath": "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe",
+  "uidUnwatchList": [],
+  "showLiveInfo": true,
+  "checkWearMedal": false,
+  "serverRoomLimit": [
+    0
+  ],
+  "serverIndex": 0,
+  "checkAllRoom": false,
+  "useObsDanmaku": true,
+  "notification": false,
+  "iftttKey": "",
+  "barkKey": "",
+  "cookies": ""
+}
+
+/**
+ * 询问并建立配置文件
+ */
+function configQuestion () {
+  return inquirer.prompt([{
+    type: 'input',
+    name: 'account',
+    message: "请输入账号：",
+  }, {
+    type: 'password',
+    message: '请输入密码：',
+    mask: '*',
+    name: 'password',
+  }, {
+    type: 'confirm',
+    message: '是否开启调试？',
+    default: false,
+    name: 'debug',
+  }, {
+    type: 'confirm',
+    message: '是否于每日0点自动重启？',
+    default: false,
+    name: 'autoRestart'
+  }, {
+    type: 'confirm',
+    message: '使用OBS弹幕工具监控？',
+    default: true,
+    name: 'useObsDanmaku',
+  }, {
+    type: 'confirm',
+    message: '佩戴牌子的主播不观看？ （戴着牌子说明你正在D TA，不需要服务器挂牌子）',
+    default: false,
+    name: 'checkWearMedal'
+  }, {
+    type: 'confirm',
+    message: '只要有粉丝牌，未关注的主播也需要监控？',
+    default: false,
+    name: 'checkAllRoom'
+  }]).then((answers) => {
+    delete answers.notificationApp
+    const userConfig = {
+      ...defaultConfig,
+      ...answers
+    }
+    setConfig({userConfig})
+    return userConfig
+  })
 }
 
 module.exports = {
@@ -106,5 +201,6 @@ module.exports = {
   getUidByUrl,
   isLiveTab,
   getConfig,
-  setConfig
+  setConfig,
+  configQuestion
 }
